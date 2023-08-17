b7be00a83e4fd64d0417ad20ffc267e4833bc097
Isaac Tonyloi <59682377+isaactony@users.noreply.github.com>
2023-08-17 22:33:19 +0300

    Update users.controller.js

--- users/controllers/users.controller.js
+++ users/controllers/users.controller.js
@@ -1,6 +1,6 @@
 const UserModel = require('../models/users.model');
 const crypto = require('crypto');
-
+//
 exports.insert = (req, res) => {
     let salt = crypto.randomBytes(16).toString('base64');
     let hash = crypto.createHmac('sha512', salt).update(req.body.password).digest("base64");
